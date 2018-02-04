057bd3e5de4d0644a77343bf53c392be0b5f1962
Tasnim Ahmed <TASNIM@Tasnims-MacBook-Air.local>
2018-02-04 21:39:55 +0600

    Project Initialized

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('userID') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">userID</label>
+
+                            <div class="col-md-6">
+                                <input id="userID" type="text" class="form-control" name="userID" value="{{ old('userID') }}" required autofocus>
+
+                                @if ($errors->has('userID'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('userID') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">
+                            <label for="role" class="col-md-4 control-label">Role</label>
+
+                            <div class="col-md-6">
+                                <label><input id ="role "type="radio" class="form-control" name="role" value="student" required autofocus>Student</label>
+                                <label><input id ="role "type="radio" class="form-control" name="role" value="teacher" required autofocus>Teacher</label>
+                                <label><input id ="role "type="radio" class="form-control" name="role" value="admin" required autofocus>Admin</label>
+                                
+
+
+                                @if ($errors->has('role'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('role') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
